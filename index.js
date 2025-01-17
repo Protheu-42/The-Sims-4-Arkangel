13fd426a9d6c5a637b85b053d775e2fdae51da95
Protheu <everaldo.sobrinho42@gmail.com>
2025-01-17 20:23:36 -0300

    fix: Small fix at code to ensure that options dont repeat

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { generosMusicais } from "./lists.js"
+import { generosMusicais } from "./lists.js";
 import { caracteristicasDosSims } from "./lists.js";
 import { topicosDeConversa } from "./lists.js";
 import { atividades } from "./lists.js";
@@ -8,49 +8,49 @@ import { lcoresDeCabelo } from "./lists.js";
 
 const GLOBALOUTPUTNUMBER = 5;
 
-export function randomPìcker(list = generosMusicais){
-    let outputList = []; 
-    for (let i = 0; i < GLOBALOUTPUTNUMBER; i++) {
-        let randomNumber = getRandomNumber(list.length)
-        if(!outputList.includes(randomNumber)){
-            outputList.push(list[randomNumber])  
-        } else{
-            i--;
-        }  
+export function randomPìcker(list = generosMusicais) {
+  let outputList = [];
+  for (let i = 0; i < GLOBALOUTPUTNUMBER; i++) {
+    let randomNumber = getRandomNumber(list.length);
+    if (!outputList.includes(list[randomNumber])) {
+      outputList.push(list[randomNumber]);
+    } else {
+      i--;
     }
-    return outputList;
-};
-
-function getRandomNumber(max) {
-    return Math.floor(Math.random() * max);
+  }
+  return outputList;
 }
 
-function resultGiver(){
-    function likesAndDislikes() {
-        const music = randomPìcker(generosMusicais);
-        const characteristcs = randomPìcker(caracteristicasDosSims);
-        const conversation = randomPìcker(topicosDeConversa);
-        const activities = randomPìcker(atividades);
-        console.log(`Gostos e Aversões
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function resultGiver() {
+  function likesAndDislikes() {
+    const music = randomPìcker(generosMusicais);
+    const characteristcs = randomPìcker(caracteristicasDosSims);
+    const conversation = randomPìcker(topicosDeConversa);
+    const activities = randomPìcker(atividades);
+    console.log(`Gostos e Aversões
             Gênero Musical: ${music}
             Características: ${characteristcs}
             Tópicos de conversa: ${conversation}
             Atividades: ${activities}
             `);
-    }
-    function turnOnsAndOffs() {
-        const lifeStyle = randomPìcker(lModosDeVida);
-        const loveCharacteristcs = randomPìcker(lCaracteristicas);
-        const hairColor = randomPìcker(lcoresDeCabelo);
-        
-        console.log(`Afeições e Repulsas
+  }
+  function turnOnsAndOffs() {
+    const lifeStyle = randomPìcker(lModosDeVida);
+    const loveCharacteristcs = randomPìcker(lCaracteristicas);
+    const hairColor = randomPìcker(lcoresDeCabelo);
+
+    console.log(`Afeições e Repulsas
             Modos de vida: ${lifeStyle}
             Características: ${loveCharacteristcs}
             Cor de Cabelo: ${hairColor}
             `);
-    }
-    likesAndDislikes();
-    turnOnsAndOffs();
+  }
+  likesAndDislikes();
+  turnOnsAndOffs();
 }
 
-resultGiver()
+resultGiver();
